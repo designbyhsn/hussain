4f2ae801f218203b05816f41b66fd25782d9174c
hussa <hussa@HUSSAIN-007>
2022-11-29 22:37:42 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Into_to_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
